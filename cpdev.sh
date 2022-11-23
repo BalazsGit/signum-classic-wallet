4748b23d3432aa4deffafc603ac9feebaa701275
deleterium <rui.santana@protonmail.com>
2022-11-23 20:58:42 -0300

    chore gitgnore and adding debug file for adminlte

--- cpdev.sh
+++ cpdev.sh
@@ -1,4 +1,4 @@
 cp src/index.html src/index.dev.html && sed -i 's/index.min.js/index.dev.js/g' src/index.dev.html
-cp node_modules/admin-lte/dist/js/adminlte.min.js* src/js/3rdparty/
+cp node_modules/admin-lte/dist/js/adminlte.* src/js/3rdparty/
 cp node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.* src/js/3rdparty/
 cp node_modules/admin-lte/dist/css/adminlte.css* src/css/
