af96e41b522121f7bb0de37bdd820b01b56ed2fb
deleterium <rui.santana@protonmail.com>
2022-12-29 23:19:07 -0300

    Adjusting raw tx to broadcast tx modal

--- src/js/brs/brs.modals.advanced.js
+++ src/js/brs/brs.modals.advanced.js
@@ -9,26 +9,11 @@ export function showRawTransactionModal (transaction) {
     $('#raw_transaction_modal_unsigned_transaction_bytes').val(transaction.unsignedTransactionBytes)
     $('#raw_transaction_modal_transaction_bytes').val(transaction.transactionBytes)
 
-    if (transaction.fullHash) {
-        $('#raw_transaction_modal_full_hash').val(transaction.fullHash)
-        $('#raw_transaction_modal_full_hash_container').show()
-    } else {
-        $('#raw_transaction_modal_full_hash_container').hide()
-    }
-
-    if (transaction.signatureHash) {
-        $('#raw_transaction_modal_signature_hash').val(transaction.signatureHash)
-        $('#raw_transaction_modal_signature_hash_container').show()
-    } else {
-        $('#raw_transaction_modal_signature_hash_container').hide()
-    }
-
     $('#raw_transaction_modal').modal('show')
 }
 
 export function formsBroadcastTransactionComplete (response, data) {
     $('#parse_transaction_form').find('.error_message').hide()
-    $('#transaction_operations_modal').modal('hide')
 }
 
 export function formsParseTransactionComplete (response, data) {
