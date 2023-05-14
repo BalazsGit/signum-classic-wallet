0e7a787c2aea8f34e159a7b42d628a79aec32328
deleterium <rui.santana@protonmail.com>
2023-05-14 10:11:03 -0300

    Bump version 1.0.0

--- src/js/brs/index.js
+++ src/js/brs/index.js
@@ -118,7 +118,7 @@ import { init } from './brs'
 import { formsClearData } from './brs.modal.cleardata'
 
 export const BRS = {
-    version: '1.0.0-beta.2',
+    version: '1.0.0',
     server: '',
     state: {},
     blocks: [],
